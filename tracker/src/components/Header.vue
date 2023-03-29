9f3711f38dfae6373df6db7b08b141a3e63f72fd
DegirmenciOmer <rumidegirmenci0@gmail.com>
2023-03-29 18:12:22 +0200

    add header, tasks, button components

---
+++ tracker/src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <AppButton text="Add Task" color="green" />
+  </header>
+</template>
+
+<script>
+import AppButton from './Button'
+
+export default {
+  name: 'AppHeader',
+  props: {
+    title: String,
+  },
+  components: { AppButton },
+}
+</script>
+
+<style>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ tracker/src/components/Tasks.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div v-for="task in tasks" :key="task.id">
+      <h3>{{ task.text }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppTasks',
+  props: {
+    tasks: Array,
+  },
+}
+</script>
+
+<style></style>
