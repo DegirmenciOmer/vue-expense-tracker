950fa94884e24788b310ea4cbe3bda4ee3bad312
DegirmenciOmer <rumidegirmenci0@gmail.com>
2023-03-30 16:26:17 +0200

    refactor crud operations to use backend

--- tracker/src/App.vue
+++ tracker/src/App.vue
@@ -28,31 +28,71 @@ export default {
     return { tasks: this.tasks, showAddTask: false }
   },
   methods: {
-    addTask(task) {
-      this.tasks = [...this.tasks, task]
+    async addTask(task) {
+      const res = await fetch('api/tasks', {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(task),
+      })
+
+      const data = await res.json()
+      console.log({ data })
+      this.tasks = [...this.tasks, data]
     },
-    deleteTask(id) {
+    async deleteTask(id) {
       if (confirm('Are you sure')) {
-        this.tasks = this.tasks.filter((task) => task.id !== id)
+        const res = await fetch(`api/tasks/${id}`, {
+          method: 'DELETE',
+        })
+
+        res.status === 200
+          ? (this.tasks = this.tasks.filter((task) => task.id !== id))
+          : alert('Delete is not successful')
       }
     },
-    toggleReminder(id) {
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id)
+
+      const updatedTask = {
+        ...taskToToggle,
+        reminder: !taskToToggle.reminder,
+      }
+
+      const res = await fetch(`api/tasks/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(updatedTask),
+      })
+
+      const data = await res.json()
+
       // this.tasks[id - 1].reminder = !this.tasks[id - 1].reminder
       // or
       this.tasks = this.tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     },
     toggleAddTask() {
       this.showAddTask = !this.showAddTask
     },
+    async fetchTasks() {
+      const res = await fetch('api/tasks')
+
+      const data = await res.json()
+
+      return data
+    },
+
+    async fetchTask(id) {
+      const res = await fetch(`api/tasks/${id}`)
+
+      const data = await res.json()
+
+      return data
+    },
   },
-  created() {
-    this.tasks = [
-      { id: 1, text: 'Doctor', day: '23 march', reminder: true },
-      { id: 2, text: 'Groceries', day: '25 march', reminder: true },
-      { id: 3, text: 'Travel', day: '23 april', reminder: false },
-    ]
+  async created() {
+    this.tasks = await this.fetchTasks()
   },
   emits: ['delete-task'],
 }
--- tracker/src/components/AddTask.vue
+++ tracker/src/components/AddTask.vue
@@ -40,7 +40,7 @@ export default {
         return
       }
       const newTask = {
-        id: Math.floor(Math.random() * 1000000),
+        // id: Math.floor(Math.random() * 1000000), //will be added by json server
         text: this.text,
         day: this.day,
         reminder: this.reminder,
