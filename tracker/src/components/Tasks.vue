a81d148d119333220ca2c31f4880b876888d2704
DegirmenciOmer <rumidegirmenci0@gmail.com>
2023-03-31 19:16:14 +0200

    reimplement logic without backend

--- tracker/src/components/Tasks.vue
+++ tracker/src/components/Tasks.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <div v-for="task in tasks" :key="task.id">
-      <AppTask
-        @toggle-reminder="$emit('toggle-reminder', task.id)"
-        @delete-task="$emit('delete-task', task.id)"
-        :task="task"
-      />
-    </div>
+    <template v-if="tasks.length > 0">
+      <div v-for="task in tasks" :key="task.id">
+        <AppTask
+          @toggle-reminder="$emit('toggle-reminder', task.id)"
+          @delete-task="$emit('delete-task', task.id)"
+          :task="task"
+        />
+      </div>
+    </template>
+    <template v-else>
+      <p>No tasks found!</p>
+    </template>
   </div>
 </template>
 
